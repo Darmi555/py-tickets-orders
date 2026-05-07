e51be618e299e3994a696accb93b1eb51ec8f3aa
Mateusz <mateusz.rudnicki05@wp.pl>
2026-05-07 15:35:39 +0200

    task4

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -71,14 +71,20 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "cinema_hall_capacity",
         )
 
+class TicketPlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
     movie = MovieListSerializer(many=False, read_only=True)
     cinema_hall = CinemaHallSerializer(many=False, read_only=True)
+    taken_places = TicketPlaceSerializer(source="tickets", many=True, read_only=True)
 
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall", "taken_places")
+
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
